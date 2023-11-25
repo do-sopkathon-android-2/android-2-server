2ad727cb72b30b8533693da0dce7dd1bb2d0bb75
sohyundoh <lyny.doh@gmail.com>
2023-11-25 18:44:53 +0900

    [init] 초기 세팅

---
+++ sopkathon-server/src/main/java/org/sopt/sopkathonserver/SopkathonServerApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.sopkathonserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopkathonServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SopkathonServerApplication.class, args);
+	}
+
+}
